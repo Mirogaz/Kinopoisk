0ea1d30ca1ede33868fe2d453be1a0df8bfd0ac3
Andrey <ver.ver.sus.sus@mail.ru>
2022-09-11 22:18:37 +0300

    Подключение базового роутинга

--- src/pages/NavBar/index.tsx
+++ src/pages/NavBar/index.tsx
@@ -1,11 +1,12 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import './NavBar.style.scss';
 
 const NavBar = () => {
     return (
         <div className='nav'>
             <div className='nav__content'>
-                <h1 className='nav__content-title'>Кинопоиск</h1>
+                <Link to='/'><h1 className='nav__content-title'>Кинопоиск</h1></Link>
             </div>
         </div>
     );
